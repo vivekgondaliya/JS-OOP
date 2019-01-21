a4252c156ee0123bb378ba2c990b789deab68efe
vivekgondaliya <vg13@zips.uakron.edu>
2019-01-21 13:15:57 -0500

    Abstraction based solution. Using Object.property() to access private data.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function Stopwatch() {
   let stopwatchRunning = false;
-  let stopwatchStopped = false;
-  let startTime, endTime;
-
-  this.duration = 0;
+  let startTime,
+    endTime,
+    duration = 0;
 
   this.start = function() {
     //ensure that clock is not running - use a flag
@@ -12,31 +11,33 @@ function Stopwatch() {
     //start the clock - run a fucntion that stores the current time in duration
     startTime = new Date();
     stopwatchRunning = true;
-    stopwatchStopped = false;
   };
 
   this.stop = function() {
     //ensure that clock is not stopped - use a flag
-    if (stopwatchStopped) throw new Error("Stopwatch already stopped...");
+    if (!stopwatchRunning) throw new Error("Stopwatch already stopped...");
 
     //stop the clock
-    endTime = new Date();
-    var timeDiff = endTime - startTime; //in ms
-    // strip the ms
-    timeDiff /= 1000;
-
-    // get seconds
-    var seconds = Math.round(timeDiff);
-    this.duration += seconds;
-
-    stopwatchStopped = true;
     stopwatchRunning = false;
+
+    endTime = new Date();
+    const seconds = (endTime - startTime) / 1000;
+    duration += seconds;
   };
 
   this.reset = function() {
     if (stopwatchRunning)
       throw new Error("Please, ensure watch is stopped to reset");
 
-    this.duration = 0;
+    startTime = null;
+    endTime = null;
+    stopwatchRunning = false;
+    duration = 0;
   };
+
+  Object.defineProperty(this, "duration", {
+    get: function() {
+      return duration;
+    }
+  });
 }
