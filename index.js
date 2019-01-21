49dfb082d1fb93cad7b1fd9083a6612c237f41ca
vivekgondaliya <vg13@zips.uakron.edu>
2019-01-20 20:10:16 -0500

    Factory vs Constructor Function definitions

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-//Factory Function
+//GOAL: Create an Object
+
+//Factory Function - returns an Object
 function createCircle(radius) {
   return {
     radius,
     draw: function() {
-      console.log("Draw");
+      console.log("Factory Draw");
     }
   };
 }
 
 const circle = createCircle(1);
 circle.draw();
+
+//Constructor Function - First letter is Capital letter.
+//'this' and 'new' keywords are used.
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("Constructor Draw");
+  };
+}
+
+const another = new Circle(1);
+another.draw();
