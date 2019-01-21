d830f6674bab2c00152223a18df254e3198660af
vivekgondaliya <vg13@zips.uakron.edu>
2019-01-20 21:11:18 -0500

    Abstraction

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Code Reset
+//Abstraction - Hide details and show the essentials. ex: DVD Player
+// Problem(s): everything is public, will create more problems in the future
+function Circle(radius) {
+  this.radius = radius;
+  this.defaultLocation = { x: 0, y: 0 };
+  this.computeOptimumLocation = function() {
+    //...
+  };
+
+  this.draw = function() {
+    computeOptimumLocation();
+    console.log("draw");
+  };
+}
+
+const circle = new Circle(1);
+circle.draw();
